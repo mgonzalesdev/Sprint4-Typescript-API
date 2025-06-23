5cd4fa8a4191e5d6ba8c795c096b82fd62df7737
Mariela Gonzales <mariela.gonzales.segobia@gmail.com>
2025-06-23 12:00:58 -0400

    Ejercicio 4: Consumo Api del Tiempo

--- src/joke.ts
+++ src/joke.ts
@@ -1,29 +1,26 @@
 export type Record = { joke: string; score: number, date: string };
 const reportAcudits: Record[] = [];
-// {
-//  joke: "...",
-//  score: 1,
-//  date: ...
-// }
 
+//https://official-joke-api.appspot.com/random_joke
 
 
 export async function getJoke() {
     try {
-        const res = await fetch('https://icanhazdadjoke.com/', {
+        const resp = await fetch('https://icanhazdadjoke.com/', {
             headers: {
                 'Accept': 'application/json',
             }
         });
 
-        if (!res.ok) {
-            throw new Error(`Error ${res.status}: ${res.statusText}`);
+        if (!resp.ok) {
+            throw new Error(`Error ${resp.status}: ${resp.statusText}`);
 
         }
-        const data = await res.json(); // data.joke contiene el texto del chiste, convierte la respuesta del servidor en un json, se colocaun await porque la funcion va ha tardar x tiempo
-        return data;
+        const data = await resp.json(); // data.joke contiene el texto del chiste, convierte la respuesta del servidor en un json, se colocaun await porque la funcion va ha tardar x tiempo
+        return data.joke;
     } catch (error) {
-        console.error('Error al consultar la API:', error);
+        console.error('Error al consultar la API: icanhazdadjoke', error);
+        return null;
     }
 }
 
@@ -31,11 +28,14 @@ export function saveAcudits(record: Record): void {
     reportAcudits.push(record);
 }
 
-export function deleteLastRecord(): void {
-    reportAcudits.pop();
+export function deleteLastRecord(text: string): boolean {
+    if (reportAcudits[reportAcudits.length - 1].joke === text) {
+        reportAcudits.pop();
+        return true;
+    } else
+        return false;
 }
 
 export function showHistory(): Record[] {
     return reportAcudits;
 }
-//export { getJoke, saveAcudits, Record };
--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,45 @@
 import { getJoke, saveAcudits, deleteLastRecord, showHistory } from './joke.ts';
+import { getForecast } from './weatherForecast.ts';
+
 import type { Record } from './joke.ts';
 
-const contentJoke = document.querySelector("#content-joke");
+const contentJoke = document.querySelector("#content-joke") as HTMLElement;
+const weather = document.querySelector("#weather") as HTMLElement;
 let isSaved: boolean = false;
+let joke: string = "";
 
-async function nextJoke() {
+async function nextJoke(): Promise<void> {
     try {
-        const data = await getJoke();
-        contentJoke.textContent = data.joke;
-        isSaved = false;
-        console.log(data.joke);
+        joke = await getJoke();
+        if (joke) {
+            isSaved = false;
+            contentJoke.textContent = joke;
+        } else
+            throw new Error(`Error al obtener el chiste.`);
+        console.log(joke);
     } catch (error) {
         // console.error('Error al obtener chiste:', error);
         contentJoke.innerHTML = `<p class="text-red-500">Error al cargar chiste ${error}</p>`;
     }
 }
 
-nextJoke();//carga el primer chiste
+async function getWeather(): Promise<void> {
+    try {
+        let dataForecast = await getForecast();
+        if (dataForecast)
+            weather.textContent = dataForecast.temperature;
+        else
+            throw new Error(`Error al obtener los datos del tiempo.`);
 
-const saveScore = (event: MouseEvent): void => {
+    } catch (error) {
+        weather.textContent = "Error al consultar el tiempo: " + error;
+        console.error("Error al consultar el tiempo:", error);
+    }
+}
+
+function saveScore(event: MouseEvent): void {
     const target = event.currentTarget as HTMLButtonElement;
     let score = parseInt(target.value);//Obtener la calificacion 
-    let divJoke = document.querySelector('#content-joke');
-    let joke: string = "";
-
-    if (divJoke) {
-        joke = divJoke.textContent || ''; //obtener el chiste
-        console.log(joke);
-    }
-
     let currentDate = new Date().toISOString();//obtener la fecha
     const newRecord: Record = { joke: joke, score: score, date: currentDate };
     if (isSaved === false) {
@@ -36,17 +47,21 @@ const saveScore = (event: MouseEvent): void => {
         isSaved = true;
         console.log(showHistory());
     } else {
-        deleteLastRecord();
-        saveAcudits(newRecord);
+        if (deleteLastRecord(joke))
+            saveAcudits(newRecord);
         console.log(showHistory());
     }
 };
 
-const btns = document.querySelectorAll('.record');
+function initApp(): void {
+    getWeather();// carga el tiempo
+    nextJoke();//carga el primer chiste
 
-btns.forEach(btn => {
-    btn.addEventListener("click", saveScore);
-});
 
-const btn = document.getElementById('next-joke');
-btn?.addEventListener('click', nextJoke);
+    document.getElementById('next-joke')?.addEventListener('click', nextJoke);
+
+    document.querySelectorAll('.record').forEach(btn =>
+        btn.addEventListener("click", saveScore)
+    );
+}
+document.addEventListener("DOMContentLoaded", initApp);

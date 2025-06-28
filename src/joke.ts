61fb62d8dd8b1d253318e752c8ad87cbaa018f7b
Mariela Gonzales <mariela.gonzales.segobia@gmail.com>
2025-06-28 05:37:41 -0400

    Ejercicio 5,6: Alternar chistes de otra Api, cambiar imagen de fondo

--- src/joke.ts
+++ src/joke.ts
@@ -1,9 +1,6 @@
 export type Record = { joke: string; score: number, date: string };
 const reportAcudits: Record[] = [];
 
-//https://official-joke-api.appspot.com/random_joke
-
-
 export async function getJoke() {
     try {
         const resp = await fetch('https://icanhazdadjoke.com/', {
@@ -14,12 +11,31 @@ export async function getJoke() {
 
         if (!resp.ok) {
             throw new Error(`Error ${resp.status}: ${resp.statusText}`);
-
         }
         const data = await resp.json(); // data.joke contiene el texto del chiste, convierte la respuesta del servidor en un json, se colocaun await porque la funcion va ha tardar x tiempo
         return data.joke;
     } catch (error) {
         console.error('Error al consultar la API: icanhazdadjoke', error);
+        return null;
+    }
+}
+
+export async function getJokeOfficialJoke() {
+    try {
+        const resp = await fetch('https://official-joke-api.appspot.com/random_joke', {
+            headers: {
+                'Accept': 'application/json',
+            }
+        });
+        if (!resp.ok) {
+            throw new Error(`Error ${resp.status}: ${resp.statusText}`);
+
+        }
+        const data = await resp.json(); 
+        let joke = `- ${data.setup} <br>- ${data.punchline}`;       
+        return joke;
+    } catch (error) {
+        console.error('Error al consultar la API: Official Joke', error);
         return null;
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,35 @@
-import { getJoke, saveAcudits, deleteLastRecord, showHistory } from './joke.ts';
+import { getJoke, getJokeOfficialJoke, saveAcudits, deleteLastRecord, showHistory } from './joke.ts';
 import { getForecast } from './weatherForecast.ts';
 
 import type { Record } from './joke.ts';
 
 const contentJoke = document.querySelector("#content-joke") as HTMLElement;
 const weather = document.querySelector("#weather") as HTMLElement;
+const weatherIcon = document.querySelector("#weatherIcon") as HTMLElement;
+const apiJokes: Array<() => Promise<string>> = [getJoke, getJokeOfficialJoke];
+const backgrounds: string[] = [
+    "bg-[url('/public/blob_1.svg')]",
+    "bg-[url('/public/blob_2.svg')]",
+    "bg-[url('/public/blob_3.svg')]",
+    "bg-[url('/public/blob_4.svg')]",
+    "bg-[url('/public/blob_5.svg')]"];
+
+let current = 0;
 let isSaved: boolean = false;
 let joke: string = "";
 
+async function getRandomApi(): Promise<string> {
+    let index = Math.floor(Math.random() * apiJokes.length);
+    return await apiJokes[index]();
+}
+
 async function nextJoke(): Promise<void> {
     try {
-        joke = await getJoke();
+        setBackground();
+        joke = await getRandomApi();
         if (joke) {
             isSaved = false;
-            contentJoke.textContent = joke;
+            contentJoke.innerHTML = joke;
         } else
             throw new Error(`Error al obtener el chiste.`);
         console.log(joke);
@@ -26,8 +42,10 @@ async function nextJoke(): Promise<void> {
 async function getWeather(): Promise<void> {
     try {
         let dataForecast = await getForecast();
-        if (dataForecast)
+        if (dataForecast) {
             weather.textContent = dataForecast.temperature;
+            weatherIcon.classList.add(weatherCodeMap[dataForecast.icon]);
+        }
         else
             throw new Error(`Error al obtener los datos del tiempo.`);
 
@@ -39,8 +57,8 @@ async function getWeather(): Promise<void> {
 
 function saveScore(event: MouseEvent): void {
     const target = event.currentTarget as HTMLButtonElement;
-    let score = parseInt(target.value);//Obtener la calificacion 
-    let currentDate = new Date().toISOString();//obtener la fecha
+    let score = parseInt(target.value);
+    let currentDate = new Date().toISOString();
     const newRecord: Record = { joke: joke, score: score, date: currentDate };
     if (isSaved === false) {
         saveAcudits(newRecord);
@@ -54,14 +72,53 @@ function saveScore(event: MouseEvent): void {
 };
 
 function initApp(): void {
-    getWeather();// carga el tiempo
-    nextJoke();//carga el primer chiste
-
+    getWeather();
+    nextJoke();
 
     document.getElementById('next-joke')?.addEventListener('click', nextJoke);
 
     document.querySelectorAll('.record').forEach(btn =>
         btn.addEventListener("click", saveScore)
     );
+    document.body.classList.add(backgrounds[current]);
 }
+
 document.addEventListener("DOMContentLoaded", initApp);
+
+type Icon = { [code: number]: string };
+const weatherCodeMap: Icon = {
+    0: "wi-day-sunny",
+    1: "wi-day-sunny-overcast",
+    2: "wi-day-cloudy",
+    3: "wi-cloudy",
+    45: "wi-fog",
+    48: "wi-fog",
+    51: "wi-sprinkle",
+    53: "wi-sprinkle",
+    55: "wi-showers",
+    56: "wi-sleet",
+    57: "wi-sleet",
+    61: "wi-rain",
+    63: "wi-rain",
+    65: "wi-rain-wind",
+    66: "wi-rain-mix",
+    67: "wi-rain-mix",
+    71: "wi-snow",
+    73: "wi-snow",
+    75: "wi-snow",
+    77: "wi-snowflake-cold",
+    80: "wi-showers",
+    81: "wi-showers",
+    82: "wi-showers",
+    85: "wi-snow",
+    86: "wi-snow",
+    95: "wi-thunderstorm",
+    96: "wi-thunderstorm",
+    99: "wi-thunderstorm"
+};
+
+function setBackground(): void {
+    document.body.classList.remove(backgrounds[current]); 
+    (current === backgrounds.length - 1) ? current = 0 : current++;
+    document.body.classList.add(backgrounds[current]);
+}
